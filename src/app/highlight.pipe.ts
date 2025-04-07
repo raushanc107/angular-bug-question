6017a760ff79ce3b1ea43ac5a15f2ebe16bb2bba
Raushan chaudhary <raushan.chaudhary@MPL-6KJ0DS3>
2025-04-07 13:17:39 +0530

    solution added

--- src/app/highlight.pipe.ts
+++ src/app/highlight.pipe.ts
@@ -15,4 +15,21 @@ export class HighlightPipe implements PipeTransform {
     }
     return result;
   }
+
+  //fixed code solution
+
+  // transform(value: string, filter: string): unknown {
+  //   let result = '';
+  //   let lowercasevalue = value.toLowerCase();
+  //   if (filter && lowercasevalue.includes(filter.toLowerCase() ?? '')) {
+  //     const index = lowercasevalue.indexOf(filter.toLowerCase() ?? '');
+  //     result += value.substring(0, index);
+  //     result +=
+  //       '<strong>' +
+  //       value.substring(index, index + filter.length) +
+  //       '</strong>';
+  //     result += value.substring(index + filter.length);
+  //   }
+  //   return result.trim() == '' ? value : result;
+  // }
 }
